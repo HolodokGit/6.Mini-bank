6c1f741747a716b911de5f65f4abaf91b387e42c
HolodokGit <super.troshnev@gmail.com>
2026-09-07 12:22:07 +0300

    feat: add Bank.getAccount method

--- services/bank.js
+++ services/bank.js
@@ -2,8 +2,9 @@
 
 import { User } from '../models/User.js';
 import { Account } from '../models/Account.js';
+import { AccountNotFoundError } from '../utils/errors.js';
 
-class Bank {
+export class Bank {
     constructor() {
         this.users = new Map();
         this.accounts = new Map();
@@ -18,8 +19,13 @@ class Bank {
     createAccount(owner, name) {
         let account = new Account(owner, name);
         this.accounts.set(account.id, account);
-        return account.balance;
+        return account;
     }
 
-    
+    getAccount(id) {
+        if(!this.accounts.has(id)) {
+            throw new AccountNotFoundError('Аккаунт не найден!');
+        }
+        return this.accounts.get(id);
+    }
 }

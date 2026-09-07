a22cb5e0937848b4922bd79a49fa1f31eeb657bd
HolodokGit <super.troshnev@gmail.com>
2026-09-07 22:57:27 +0300

    test: add Bank-tests

---
+++ tests/bank-test.js
@@ -0,0 +1,58 @@
+"use strict"
+
+import { Bank } from '../services/bank.js';
+import { AccountNotFoundError, InsufficientFundsError } from '../utils/errors.js';
+
+
+describe("Bank", function() {
+    it('Создание пользователя', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        assert.equal(user.name, 'John');
+    });
+
+    it('Создание счета', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        let account = bank.createAccount(user, 'Основной');
+        assert.equal(account.owner, user);
+        assert.equal(account.name, 'Основной');
+    });
+
+    it('Пополнение', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        let account = bank.createAccount(user, 'Основной');
+        bank.deposit(account.id, 500);
+        assert.equal(account.balance, 500);
+    });
+
+    it('Снятие', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        let account = bank.createAccount(user, 'Основной');
+        bank.deposit(account.id, 500);
+        bank.withdraw(account.id, 200);
+        assert.equal(account.balance, 300);
+    });
+
+    it('Ошибка "недостаточно средств"', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        let account = bank.createAccount(user, 'Основной');
+        assert.throws(
+            () => bank.withdraw(account.id, 200),
+            InsufficientFundsError
+        );
+    });
+
+    it('Ошибка "аккаунт не найден"', function() {
+        let bank = new Bank();
+        let user = bank.createUser('John');
+        let account = bank.createAccount(user, 'Основной');
+        assert.throws(
+            () => bank.getAccount('cdsacsda'),
+            AccountNotFoundError
+        );
+    });
+});
